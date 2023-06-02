e1ca313ea3df6fe15e729a001312b487c048fdad
EddieWongED <aolaeddie@gmail.com>
2023-06-02 19:41:42 +0800

    feat: Add mutex to mongodb

--- src/structures/database/GuildDataHandler.ts
+++ src/structures/database/GuildDataHandler.ts
@@ -1,21 +1,60 @@
 import type { GuildData } from "../../data/guild/GuildData.js";
 import { client } from "../../index.js";
+import type { MutexInterface } from "async-mutex";
+import { Mutex, withTimeout } from "async-mutex";
 
 export default class GuildDataHandler {
-    constructor(public guildId: string) {}
+    readWriteMutex: MutexInterface = new Mutex();
+    readMutex: MutexInterface = new Mutex();
+    readCount = 0;
+
+    constructor(public guildId: string) {
+        this.readWriteMutex = withTimeout(this.readWriteMutex, 2000);
+        this.readMutex = withTimeout(this.readMutex, 2000);
+    }
 
     async get<Key extends keyof GuildData>(
         key: Key
     ): Promise<GuildData[Key] | null> {
+        // Readers–writers problem
+        // Acquire resource lock
+        await this.readMutex.acquire();
+        this.readCount++;
+        if (this.readCount === 1) {
+            await this.readWriteMutex.acquire();
+        }
+        this.readMutex.release();
+
+        // Perform reading
         const guildData = await client.database.guildData.read(this.guildId);
         if (!guildData) return null;
-        return guildData[key];
+        const result = guildData[key];
+
+        // Release resource lock
+        await this.readMutex.acquire();
+        this.readCount--;
+        if (this.readCount === 0) {
+            this.readWriteMutex.release();
+        }
+        this.readMutex.release();
+        return result;
     }
 
     async update<Key extends keyof GuildData>(
         key: Key,
         value: GuildData[Key]
     ): Promise<GuildData | null> {
-        return await client.database.guildData.update(this.guildId, key, value);
+        // Acquire resource lock
+        await this.readWriteMutex.acquire();
+
+        const result = await client.database.guildData.update(
+            this.guildId,
+            key,
+            value
+        );
+
+        // Release resource lock
+        this.readWriteMutex.release();
+        return result;
     }
 }
--- src/structures/database/GuildStatesHandler.ts
+++ src/structures/database/GuildStatesHandler.ts
@@ -1,27 +1,62 @@
 import type { GuildStates } from "../../data/guild/GuildStates.js";
 import { client } from "../../index.js";
+import type { MutexInterface } from "async-mutex";
+import { Mutex, withTimeout } from "async-mutex";
 
 export default class GuildStatesHandler {
-    constructor(public guildId: string) {}
+    readWriteMutex: MutexInterface = new Mutex();
+    readMutex: MutexInterface = new Mutex();
+    readCount = 0;
+
+    constructor(public guildId: string) {
+        this.readWriteMutex = withTimeout(this.readWriteMutex, 2000);
+        this.readMutex = withTimeout(this.readMutex, 2000);
+    }
 
     async get<Key extends keyof GuildStates>(
         key: Key
     ): Promise<GuildStates[Key] | null> {
+        // Readers–writers problem
+        // Acquire resource lock
+        await this.readMutex.acquire();
+        this.readCount++;
+        if (this.readCount === 1) {
+            await this.readWriteMutex.acquire();
+        }
+        this.readMutex.release();
+
+        // Perform reading
         const guildStates = await client.database.guildStates.read(
             this.guildId
         );
         if (!guildStates) return null;
-        return guildStates[key];
+        const result = guildStates[key];
+
+        // Release resource lock
+        await this.readMutex.acquire();
+        this.readCount--;
+        if (this.readCount === 0) {
+            this.readWriteMutex.release();
+        }
+        this.readMutex.release();
+        return result;
     }
 
     async update<Key extends keyof GuildStates>(
         key: Key,
         value: GuildStates[Key]
     ): Promise<GuildStates | null> {
-        return await client.database.guildStates.update(
+        // Acquire resource lock
+        await this.readWriteMutex.acquire();
+
+        const result = await client.database.guildStates.update(
             this.guildId,
             key,
             value
         );
+
+        // Release resource lock
+        this.readWriteMutex.release();
+        return result;
     }
 }
